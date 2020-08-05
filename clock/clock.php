900f1ad2259f473fb31622ec2826abf9e55495f9
Symeon Quimby <kershoc@gmail.com>
2020-08-04 22:38:11 -0400

    clock exercise

---
+++ clock/clock.php
@@ -0,0 +1,30 @@
+<?php
+class Clock
+{
+    const DAY_IN_MINUTES = 1440;
+    private $minutes;
+
+    public function __construct(int $hour, int $minutes = 0)
+    {
+        $this->minutes = ($hour*60) + $minutes;
+        while ($this->minutes < 0) {
+            $this->minutes += self::DAY_IN_MINUTES;
+        }
+        $this->minutes %= self::DAY_IN_MINUTES;
+    }
+
+    public function add(int $minutes):Clock
+    {
+        return new self(0, $this->minutes + $minutes);
+    }
+
+    public function sub(int $minutes):Clock
+    {
+        return new self(0, $this->minutes - $minutes);
+    }
+
+    public function __toString():string
+    {
+        return sprintf("%02d:%02d",$this->minutes/60,$this->minutes%60);
+    }
+}
