f17cb6d36fc200d74149289a8e8f223245329421
Symeon Quimby <kershoc@gmail.com>
2020-09-01 23:22:50 -0400

    meetup exercise

---
+++ meetup/meetup.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+define('MONTHS', [
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+]);
+
+function meetup_day(int $year, int $month, string $descriptor, string $day): DateTimeImmutable
+{
+    $date_string = $descriptor . ' ' . $day . ' of ' . MONTHS[$month] . ' ' . $year;
+
+    if ($descriptor === 'teenth') {
+        $date_string = implode('/', [$month, get_teenth($day, $month, $year), $year]);
+    }
+    
+    return new DateTimeImmutable($date_string);
+}
+
+function get_teenth(string $day, int $month, int $year): int
+{
+    foreach (range(13,19) as $dom) {
+        $date = implode('/', [$month, $dom, $year]);
+        $dow = date('l', strtotime($date));
+        if ($day === $dow) return $dom;
+    }
+}
