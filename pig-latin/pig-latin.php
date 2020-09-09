ab2231876dfe1fe242b2c04f4e7dadaf036252f9
Symeon Quimby <kershoc@gmail.com>
2020-09-08 20:30:24 -0400

    pig-latin exercise

---
+++ pig-latin/pig-latin.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+function translate(string $input): string
+{
+    $translator = new IgpayAtinlayAnslatortay($input);
+    return $translator->translate();
+}
+
+class IgpayAtinlayAnslatortay
+{
+    private const SPLIT_REGEX = "/\b(s?qu|(?!yt|xr)[^aeiou ]+|)(\w+)/i";
+    private $input = '';
+
+    public function __construct(string $input)
+    {
+        $this->input = $input;
+    }
+
+    public function translate(): string
+    {
+        return preg_replace(self::SPLIT_REGEX, "$2$1ay", $this->input);
+    }
+}
