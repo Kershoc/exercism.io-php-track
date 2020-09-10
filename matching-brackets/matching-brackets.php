d6ade6f3bc528b62e7f0d54737b538363c57d8af
Symeon Quimby <kershoc@gmail.com>
2020-09-10 18:25:42 -0400

    matching-brackets exercise

---
+++ matching-brackets/matching-brackets.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+function brackets_match(string $input): bool
+{
+    $brackets = new MatchingBrackets($input);
+    return $brackets->isValid();
+}
+
+class MatchingBrackets
+{
+    private const BRACKETS = [
+        '['=>']',
+        '{'=>'}',
+        '('=>')'
+    ];
+    private $nest = [];
+    private $input = '';
+
+    public function __construct(string $input)
+    {
+        $this->input = $input;
+    }
+
+    public function isValid(): bool
+    {
+        foreach(mb_str_split($this->input) as $char) {
+            switch(true):
+                case(in_array($char, array_keys(self::BRACKETS))):
+                    array_push($this->nest, $char);
+                    break;
+                case(in_array($char, self::BRACKETS)):
+                    $check = array_pop($this->nest);
+                    if ($check && self::BRACKETS[$check] !== $char) {
+                        return false;
+                    }
+                    break;
+            endswitch;
+        }
+
+        return empty($this->nest);
+    }
+}
