c73d8e01af00ef7089650af87be60293b84c80d6
Symeon Quimby <kershoc@gmail.com>
2020-10-12 21:51:39 -0400

    perfect-numbers exercise

---
+++ perfect-numbers/perfect-numbers.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+function getClassification(int $number): string
+{
+    $classification = new NicomachusClassification($number);
+    return $classification->getClassification();
+}
+
+class NicomachusClassification
+{
+    const NUMBER_PERFECT = 'perfect';
+    const NUMBER_ABUNDANT = 'abundant';
+    const NUMBER_DEFICIENT = 'deficient';
+    
+    public function __construct(int $number)
+    {
+        if ($number < 1) {
+            throw new InvalidArgumentException();
+        }
+        $this->number = $number;
+    }
+
+    public function getClassification(): string
+    {
+        switch(($sum = $this->aliquotSum()) <=> $this->number) {
+            case 0:
+                if ($sum !== 1) return self::NUMBER_PERFECT;
+            case -1:
+                return self::NUMBER_DEFICIENT;
+            case 1:
+                return self::NUMBER_ABUNDANT;
+        }
+    }
+
+    private function aliquotSum(): int
+    {
+        $sum = 1;
+        for($i=2; $i**2 < $this->number; $i++) {
+            if($this->number%$i === 0){
+                $sum += $i + $this->number/$i;
+            }
+        }
+        return $sum;
+    }
+}
