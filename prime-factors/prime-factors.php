30ec6c05e34b68e4a4452a9b6ca33524d6f6a47a
Symeon Quimby <kershoc@gmail.com>
2020-11-04 21:13:54 -0500

    prime-factors exercise

---
+++ prime-factors/prime-factors.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+function factors(int $num): array 
+{
+    $divisor = 2;
+    $return = [];
+    while ($num > 1) {
+        if ($num % $divisor === 0) {
+            array_push($return, $divisor);
+            $num /= $divisor;
+        } else {
+            $divisor++;
+        }
+    }
+    return $return;
+}
