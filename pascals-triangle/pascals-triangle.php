635aa2e58843e82da684bd195ff3ea5f06e1522f
Symeon Quimby <kershoc@gmail.com>
2020-11-16 23:36:49 -0500

    fix issues raised by phpstan

--- pascals-triangle/pascals-triangle.php
+++ pascals-triangle/pascals-triangle.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
  * The tests that came with the exercise encourages bad design.
  * I didn't like it, so changed the tests.
 */
+
+/** @return array<array<int>> */
 function pascalsTriangleRows(int $rows): array
 {
     if ($rows < 0) {
@@ -16,8 +18,8 @@ function pascalsTriangleRows(int $rows): array
         $C = 1;// used to represent C(line, i)  
         for ($i = 1; $i <= $line; $i++) {  
             $row[] = $C;
-            $C = $C * ($line - $i) / $i;  
-        }  
+            $C = intval($C * ($line - $i) / $i);
+        }
         $triangle[] = $row;
     }
 
