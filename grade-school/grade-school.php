8c5058e452893bcbd7eca3a96101ad4cf3375537
Symeon Quimby <kershoc@gmail.com>
2020-12-23 21:49:55 -0500

    grade-school exercise

---
+++ grade-school/grade-school.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+class School
+{
+    private $roster = [];
+    public function add(string $student, int $grade): void
+    {
+        $this->roster[$grade][] = $student;
+    }
+
+    public function grade(int $grade): array
+    {
+        return $this->roster[$grade] ?? [];
+    }
+
+    public function rosterByGradeAlphabetical(): array
+    {
+        $roster = $this->roster;
+        foreach ($roster as &$students) {
+            sort($students);
+        }
+        asort($roster);
+        return $roster;
+    }
+}
