a0dc82b6c55e7b4c6a69250bf5980633ab384892
Symeon Quimby <kershoc@gmail.com>
2021-03-10 22:50:16 -0500

    sum-of-multiples exercise

---
+++ sum-of-multiples/sum-of-multiples.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+function sumOfMultiples(int $num, array $multiples): int
+{
+    $multiples = array_filter($multiples);
+
+    $result = [];
+    foreach (range(1, $num-1) as $check) {
+        foreach ($multiples as $m) {
+            if ($check%$m == 0 ) {
+                $result[] = $check;
+                break;
+            }
+        }
+    }
+
+    return array_sum($result);
+}
